ba95a498f71e499b1265a79901b8a09892430880
kimsongsub <busanbak2000@naver.com>
2022-06-17 19:12:01 +0900

    📃 change file name
    paint -> sketch
    sketch -> paint

---
+++ js/sketch.js
@@ -0,0 +1,31 @@
+const modalHandleIconArray = document.querySelectorAll(".modal-handle-icon");
+const wrapModalSideMenu = document.querySelector(".wrap-modal-side-menu");
+const body = document.querySelector("body");
+
+modalHandleIconArray.forEach((modalHandleIcon) => {
+  modalHandleIcon.addEventListener("click", function () {
+    body.classList.toggle("disable-scroll");
+    wrapModalSideMenu.classList.toggle("hide-element");
+  });
+});
+
+const sketchSection = document.querySelector(".sketch-section");
+const sampleSketchNonColoredArray = document.querySelectorAll(
+  ".sample-sketch-non-colored"
+);
+
+sampleSketchNonColoredArray.forEach((sample) => {
+  sample.addEventListener("click", async function () {
+    sketchSection.innerHTML = await fetchHtmlAsText("/page/sketch.html");
+    conveyClickedSketchSrc(sample);
+  });
+});
+
+async function fetchHtmlAsText(url) {
+  return await (await fetch(url)).text();
+}
+
+function conveyClickedSketchSrc(sample) {
+  const clickedSketch = document.querySelector("#clickedSketch img");
+  clickedSketch.src = sample.src;
+}
