4150fb197e146d7ad7a2947d5b143f971444d6d2
seahare-slug <70680118+seahare-slug@users.noreply.github.com>
2022-06-29 01:01:04 +0900

    🔰 update paint.js paint..html
    Add paint-menu-btn-hint

--- js/paint.js
+++ js/paint.js
@@ -27,3 +27,37 @@ zoomPlus.addEventListener("click", function () {
   zoomCurrentValue.innerText = Number(zoomCurrentValue.innerText) + 10;
 });
 
+const paintExitBtn = document.querySelector("#paintExitBtn");
+const paintExitHint = document.querySelector("#paintExitHint");
+
+paintExitBtn.addEventListener("mouseenter", viewExitHint);
+paintExitBtn.addEventListener("mouseleave", hideExitHint);
+
+function viewExitHint() {
+  paintExitHint.classList.remove("hide-element");
+  paintExitHint.style.top =
+    paintExitBtn.getBoundingClientRect().top + 40 + "px";
+  paintExitHint.style.left = paintExitBtn.getBoundingClientRect().left + "px";
+}
+
+function hideExitHint() {
+  paintExitHint.classList.add("hide-element");
+}
+
+const paintColorModelBtn = document.querySelector("#paintColorModelBtn");
+const paintColorModelHint = document.querySelector("#paintColorModelHint");
+
+paintColorModelBtn.addEventListener("mouseenter", viewColorModelHint);
+paintColorModelBtn.addEventListener("mouseleave", hideColorModelHint);
+
+function viewColorModelHint() {
+  paintColorModelHint.classList.remove("hide-element");
+  paintColorModelHint.style.top =
+    paintColorModelBtn.getBoundingClientRect().top - 40 + "px";
+  paintColorModelHint.style.left =
+    paintColorModelBtn.getBoundingClientRect().left + "px";
+}
+
+function hideColorModelHint() {
+  paintColorModelHint.classList.add("hide-element");
+}
