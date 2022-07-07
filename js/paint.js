b561659fb458f47ffa4c09661d4d26191139a971
kimsongsub <busanbak2000@naver.com>
2022-07-07 22:17:00 +0900

    🔰 update Btn hint

--- js/paint.js
+++ js/paint.js
@@ -27,11 +27,19 @@ zoomPlus.addEventListener("click", function () {
   zoomCurrentValue.innerText = Number(zoomCurrentValue.innerText) + 10;
 });
 
+const paintColorModelBtn = document.querySelector("#paintColorModelBtn");
+const paintColorModelHint = document.querySelector("#paintColorModelHint");
 const paintExitBtn = document.querySelector("#paintExitBtn");
 const paintExitHint = document.querySelector("#paintExitHint");
+// const autoPaintBtn = document.querySelector("#autoPaintBtn");
+const autoPaintBtnHint = document.querySelector("#autoPaintBtnHint");
 
+paintColorModelBtn.addEventListener("mouseenter", viewColorModelHint);
+paintColorModelBtn.addEventListener("mouseleave", hideColorModelHint);
 paintExitBtn.addEventListener("mouseenter", viewExitHint);
 paintExitBtn.addEventListener("mouseleave", hideExitHint);
+autoPaintBtn.addEventListener("mouseenter", viewAutoPaintBtnHint);
+autoPaintBtn.addEventListener("mouseleave", hideAutoPaintBtnHint);
 
 function viewExitHint() {
   paintExitHint.classList.remove("hide-element");
@@ -44,12 +52,6 @@ function hideExitHint() {
   paintExitHint.classList.add("hide-element");
 }
 
-const paintColorModelBtn = document.querySelector("#paintColorModelBtn");
-const paintColorModelHint = document.querySelector("#paintColorModelHint");
-
-paintColorModelBtn.addEventListener("mouseenter", viewColorModelHint);
-paintColorModelBtn.addEventListener("mouseleave", hideColorModelHint);
-
 function viewColorModelHint() {
   paintColorModelHint.classList.remove("hide-element");
   paintColorModelHint.style.top =
@@ -60,4 +62,16 @@ function viewColorModelHint() {
 
 function hideColorModelHint() {
   paintColorModelHint.classList.add("hide-element");
+}
+
+function viewAutoPaintBtnHint() {
+  autoPaintBtnHint.classList.remove("hide-element");
+  autoPaintBtnHint.style.top =
+    autoPaintBtn.getBoundingClientRect().top - 60 + "px";
+  autoPaintBtnHint.style.left =
+    autoPaintBtn.getBoundingClientRect().left + "px";
+}
+
+function hideAutoPaintBtnHint() {
+  autoPaintBtnHint.classList.add("hide-element");
 }
